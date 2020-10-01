918ba2ea01d696e78b29c392529bfb9da702e109
VINODEA Richard <richard.vinodeaa@gmail.com>
2020-10-01 17:42:23 +0200

    add get and post method and refacto html css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'wildcodeschool';
+export class AppComponent implements OnInit {
+  getUrl: string = 'http://localhost/get.php';
+  postUrl: string = 'http://localhost/post.php';
+
+  array = [];
+
+  reactiveForm = new FormGroup({
+    name: new FormControl(),
+  });
+
+  constructor(private httpClient: HttpClient, private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.getMethod();
+  }
+
+  postMethod() {
+    let myFormData = new FormData();
+    myFormData.append('name', this.reactiveForm.value.name);
+
+    return this.httpClient.post(this.postUrl, myFormData).subscribe(
+      () => this.getMethod(),
+      (error) => console.log(error)
+    );
+  }
+
+  getMethod() {
+    this.httpClient.get(this.getUrl).subscribe(data => {
+      this.array.push(data);
+    }, error => console.error(error));
+  };
+
+  onSubmit() {
+    this.postMethod();
+  };
 }
